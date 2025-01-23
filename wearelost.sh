b96133a6a143696b36fa1a874457f4b4b39182f2
tieniscoding <tnpwn@proton.me>
2025-01-23 03:07:26 -0600

    Update wearelost.sh

--- wearelost.sh
+++ wearelost.sh
@@ -2,41 +2,48 @@
 
 
 # Flush all existing iptables rules
-iptables -F  
-iptables -X  
-# Block all incoming SSH traffic
-iptables -A INPUT -p tcp --dport 22 -j DROP
-
-# Block all outgoing SSH traffic
-iptables -A OUTPUT -p tcp --dport 22 -j DROP
-
-# Block forwarded SSH traffic
-iptables -A FORWARD -p tcp --dport 22 -j DROP 
-
-# Set default policies to DROP for all chains
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
-
+kill() {
+	iptables -F  
+	iptables -X  
+	# Block all incoming SSH traffic
+	iptables -A INPUT -p tcp --dport 22 -j DROP
+	
+	# Block all outgoing SSH traffic
+	iptables -A OUTPUT -p tcp --dport 22 -j DROP
+	
+	# Block forwarded SSH traffic
+	iptables -A FORWARD -p tcp --dport 22 -j DROP 
+	
+	# Set default policies to DROP for all chains
+	iptables -P INPUT DROP
+	iptables -P FORWARD DROP
+	iptables -P OUTPUT DROP
+}	
 
 off_adapter(){
-	for iface in $(glob /sys/class/net | grep -v lo); do
-	  sudo ip link set "$iface" down
+	for iface in $(ls /sys/class/net | grep -v lo); do
+	  sudo ip link set $iface down
 	done
 }
 
 on_adapter(){	
-	for iface in $(glob /sys/class/net | grep -v lo); do
-	  sudo ip link set "$iface" up
+	for iface in $(ls /sys/class/net | grep -v lo); do
+	  sudo ip link set $iface up
 	done
 }
 
 
 case "$1" in
 	option1)
-	        off_adapter
-	        ;;
+ 		echo "turning offfffff.......zzzzzzzzzzzzzzzz"
+		off_adapter()
+	 	;;
   	option2)
-   		on_adapter
-     	               ;;
+   		echo "turning onnnnnnn.......zzzzzzzzzzzzzzzz"
+   		on_adapter()
+     		;;
+       option3)
+       		echo "adding iptables rulezzzzzz"
+       		kill
+	 	;;
 esac
